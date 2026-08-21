d0069547623c9c2b96f1a631b56cb39d88ba5daa
ShreshthaAgnihotri <dheerajjhs44@gmail.com>
2026-08-22 00:19:50 +0530

    FINDING HIGHEST PRIME NUMBER

---
+++ highest_prime_number.py
@@ -0,0 +1,22 @@
+n = int(input("Enter a number:"))
+highest = 0
+while n>0:
+   digit = n%10
+   if digit>highest:
+    if digit == 2 or digit ==3 or digit == 5 or digit == 7:
+        highest = digit   
+   n = n//10
+if highest == 0:
+    print("There is no prime digit in the given number")
+else:
+    print("The highest prime digit is:", highest)
+
+
+    
+
+
+
+
+
+
+
