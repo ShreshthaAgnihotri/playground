7ff189384e4c32bb11e524b86a695e877c8c0e68
ShreshthaAgnihotri <dheerajjhs44@gmail.com>
2026-08-23 22:52:33 +0530

    HIGHEST PRIME NUMBER

--- highest_prime_number.py
+++ highest_prime_number.py
@@ -1,15 +1,20 @@
-n = int(input("Enter a number:"))
-highest = 0
-while n>0:
-   digit = n%10
-   if digit>highest:
-    if digit == 2 or digit ==3 or digit == 5 or digit == 7:
-        highest = digit   
-   n = n//10
-if highest == 0:
-    print("There is no prime digit in the given number")
-else:
-    print("The highest prime digit is:", highest)
+#This function takes a number as input and finds the highest prime digit in that number. If there are no prime digits, it informs the user accordingly.
+
+def highest_prime_number():
+    n = int(input("Enter a number:"))
+    highest = 0
+    while n>0:
+        digit = n%10
+        if digit>highest:
+            if digit == 2 or digit ==3 or digit == 5 or digit == 7:
+                highest = digit   
+        n = n//10
+    if highest == 0:
+        print("There is no prime digit in the given number")
+    else:
+        print("The highest prime digit is:", highest)
+
+highest_prime_number()
 
 
     
---
+++ op.py
@@ -0,0 +1,21 @@
+n = int(input("ENTER A NUMBER:"))
+oddcount = 0
+evencount = 0
+zerocount = 0
+while n>0:
+    digit = n%10
+    if digit%2 == 0 and digit!=0:
+        evencount+=1
+    elif digit%2 == 1:
+        oddcount+=1
+    elif digit ==0:
+        zerocount+=1
+    n = n//10
+print("Number of even digits are : " ,evencount)     
+print("Number of odd digits are : " ,oddcount)     
+print("Number of zeroes are : " ,zerocount)     
+
+   
+
+        
+
